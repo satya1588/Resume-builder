606397f9adedc483a4fe18115d1c42a027fa8fc1
Satya <gsatyam1588@gmail.com>
2023-07-08 15:58:52 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TopNav from './components/TopNav';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <Home/>
+      </div>
+  );
+}
+
+export default App;
